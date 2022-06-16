51aeea890f25595c0bdc226a8b0a88b2949fed69
Nurdayana Muhd Faisal <dam20@doc.ic.ac.uk>
2022-06-16 18:11:10 +0100

    Added front end code to display health bar [dam20, ac320]

--- revzen/app/src/main/java/revzen/app/FailActivity.kt
+++ revzen/app/src/main/java/revzen/app/FailActivity.kt
@@ -4,11 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 
 class FailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fail)
+
+        //API call to set health variable
+        val health = 2
+        val healthBar = findViewById<ImageView>(R.id.imageView2)
+        val image = when (health){
+            3 -> R.drawable.heart3
+            2 -> R.drawable.heart2
+            1 -> R.drawable.heart1
+            0 -> R.drawable.heart0
+            else -> R.drawable.heart3
+        }
+        healthBar.setImageResource(image);
     }
 
     override fun onBackPressed() {
