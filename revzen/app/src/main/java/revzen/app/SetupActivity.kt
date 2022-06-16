bcd4fd98e3089e629272551066f588e8bd833633
Miles Nash <mapnash@gmail.com>
2022-06-16 17:30:45 +0100

    Corrected string format for times and hides recent options if none available [ mn820 ]

--- revzen/app/src/main/java/revzen/app/SetupActivity.kt
+++ revzen/app/src/main/java/revzen/app/SetupActivity.kt
@@ -7,36 +7,52 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 
 class SetupActivity : AppCompatActivity() {
     private val studyLengths = listOf(30, 45, 60, 90, 120)
     private val breakLengths = listOf(5, 10, 15, 20, 25, 30)
-    // RESULT OF API REQUEST STORED IN THESE VARIABLES //
-    private val recentStudyTime = 90
-    private val recentBreakTime = 10
+
+    private var recentStudyTime = 150
+    private var recentBreakTime = 10
 
     private lateinit var studySpinner: Spinner
     private lateinit var breakSpinner: Spinner
+    private lateinit var recentView: TextView
     private lateinit var studyButton: Button
     private lateinit var breakButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val studyStrings = studyLengths.map { t -> timeFormat(t) }
         val breakStrings = breakLengths.map { t -> timeFormat(t) }
-        val studyButtonText = "STUDY: " + timeFormat(recentStudyTime)
-        val breakButtonText = "BREAK: " + timeFormat(recentBreakTime)
+        val hasRecentHistory = true
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
 
         studySpinner = findViewById(R.id.studyLengthSpinner)
         breakSpinner = findViewById(R.id.breakLengthSpinner)
+        recentView = findViewById(R.id.recentView)
+        studyButton = findViewById(R.id.setupStudyButton)
+        breakButton = findViewById(R.id.setupBreakButton)
         studySpinner.adapter =
             ArrayAdapter(this, android.R.layout.simple_spinner_item, studyStrings)
         breakSpinner.adapter =
             ArrayAdapter(this, android.R.layout.simple_spinner_item, breakStrings)
 
-        studyButton = findViewById(R.id.setupStudyButton)
-        breakButton = findViewById(R.id.setupBreakButton)
+        if (hasRecentHistory) {
+            //recentStudyTime = API_GET
+            //recentBreakTime = API_GET
+            studyButton.visibility = View.VISIBLE
+            breakButton.visibility = View.VISIBLE
+            recentView.visibility = View.VISIBLE
+        } else {
+            studyButton.visibility = View.GONE
+            breakButton.visibility = View.GONE
+            recentView.visibility = View.INVISIBLE
+        }
+
+        val studyButtonText = "STUDY:    " + timeFormat(recentStudyTime)
+        val breakButtonText = "BREAK:  " + timeFormat(recentBreakTime)
         studyButton.text = studyButtonText
         breakButton.text = breakButtonText
     }
@@ -66,10 +82,14 @@ class SetupActivity : AppCompatActivity() {
         return if (hours < 1) {
             "$mins MINS"
         } else {
+            var hourRep = "HOURS"
+            if (hours == 1) {
+                hourRep = "HOUR"
+            }
             if (mins > 0) {
-                "$hours HOURS $mins MINS"
+                "$hours $hourRep $mins MINS"
             } else {
-                "$hours HOURS"
+                "$hours $hourRep"
             }
         }
     }
